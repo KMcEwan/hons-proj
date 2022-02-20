f8f7cd58f701ff89b5d5d9de259e5ad02ad83a87
KMcEwan <kmce404@gmail.com>
2022-02-20 18:20:15 +0000

    shader changed

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -19,7 +19,21 @@ public class cameraController : MonoBehaviour
     List<GameObject> hitObjects = new List<GameObject>();
 
     Ray cameraToPlayerRay;
+    public enum SurfaceType
+    {
+        Opaque,
+        Transparent
+    }
 
+    public enum BlendMode
+    {
+        Alpha,
+        Premultiply,
+        Additive,
+        Multiply
+    }
+
+    public Material transparentMaterial;
 
 
 
@@ -65,11 +79,65 @@ public class cameraController : MonoBehaviour
                     {
                         transparentObjects.Add(hits[i].transform.gameObject);
                     }
+                    // targetObject.material.SetFloat("_Mode", 3f);
 
-                    objectHitMaterial = hits[i].transform.gameObject.GetComponent<Renderer>().material;
+
+                     objectHitMaterial = hits[i].transform.gameObject.GetComponent<Renderer>().material;
+                    //MeshRenderer renderer = hits[i].transform.gameObject.GetComponent<MeshRenderer>();
+
+                    //Material originalMaterial = renderer.sharedMaterial; //sharedMaterial
+
+                    //Material transMaterial = new Material(transparentMaterial);
+                    //transMaterial.CopyPropertiesFromMaterial(originalMaterial);
+                    //oldColour = transMaterial.color;
+                    //newColour = new Color(oldColour.r, oldColour.g, oldColour.b, 0.5f);
+                    //transMaterial.SetColor("_Color", newColour);
+
+                    //renderer.sharedMaterial = transMaterial;
+
+               
+                    objectHitMaterial.SetFloat("_Surface", 1.0f);
+                    objectHitMaterial.SetFloat("_Blend", 0f);
+                    //objectHitMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                    //objectHitMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    //objectHitMaterial.EnableKeyword("_ALPHABLEND_ON");
+                    //objectHitMaterial.SetOverrideTag("RenderType", "Transparent");
+                    //oldColour = objectHitMaterial.color;
+                    //newColour = new Color(oldColour.r, oldColour.g, oldColour.b, 0.1f);
+                    //objectHitMaterial.SetColor("_BaseColor", newColour);
+                    objectHitMaterial.SetOverrideTag("RenderType", "Transparent");
+                    objectHitMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                    objectHitMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    //objectHitMaterial.SetInt("_ZWrite", 0);
+                    //objectHitMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                    objectHitMaterial.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                    objectHitMaterial.SetShaderPassEnabled("ShadowCaster", false);
                     oldColour = objectHitMaterial.color;
                     newColour = new Color(oldColour.r, oldColour.g, oldColour.b, 0.5f);
-                    objectHitMaterial.SetColor("_Color", newColour);
+                    objectHitMaterial.SetColor("_BaseColor", newColour);
+
+
+                    // renderer.sharedMaterial.SetColor("_Colour", new Color(1.0f,1.0f,1.0f,0.5f));
+                    //Color oldColour2 = renderer.sharedMaterial.color;
+                    //Color newColour2 = new Color(oldColour2.r, oldColour2.g, oldColour2.b, 0.5f);
+                    //objectHitMaterial.SetColor("_Color", newColour2);
+
+                    //objectHitMaterial.SetFloat("_Mode", 3);
+                    //objectHitMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                    //objectHitMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    //objectHitMaterial.EnableKeyword("_ALPHABLEND_ON");
+                    //objectHitMaterial.renderQueue = 3000;
+
+
+                    //objectHitMaterial.SetFloat("_Surface", (float)SurfaceType.Transparent);
+                    //objectHitMaterial.SetFloat("_Blend", (float)BlendMode.Alpha);
+
+
+
+
+                    //oldColour = objectHitMaterial.color;
+                    //newColour = new Color(oldColour.r, oldColour.g, oldColour.b, 0.5f);
+                    //objectHitMaterial.SetColor("_Color", newColour);
                 }
 
 
@@ -82,16 +150,33 @@ public class cameraController : MonoBehaviour
             if (hitObjects.Contains(transparentObjects[i]))
             {
                 Debug.Log("OBJECTS IN LIST");
-                return;               
+                //return;
+                continue;
             }
             else
             {
                 Material transparentMat = transparentObjects[i].GetComponent<Renderer>().material;
+
+
+
+
                 Color transparentMatOldColour = transparentMat.color;
                 Color transparentMatnewColour = new Color(transparentMatOldColour.r, transparentMatOldColour.g, transparentMatOldColour.b, 1f);
                 transparentMat.SetColor("_Color", transparentMatnewColour);
                 transparentObjects.Remove(transparentObjects[i]);
                 Debug.Log("Not in list");
+
+                transparentMat.SetFloat("_Surface", 0);
+
+
+                transparentMat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                transparentMat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                transparentMat.SetInt("_ZWrite", 1);
+                transparentMat.DisableKeyword("_ALPHATEST_ON");
+                transparentMat.DisableKeyword("_ALPHABLEND_ON");
+                transparentMat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                transparentMat.renderQueue = -1;
+
 
             }
 
